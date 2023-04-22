a58ad96adb7147888e2f7cea9ea36370b7f44b94
rexsolis7458 <rexbryan.solis.20@usjr.edu.ph>
2023-04-22 21:13:13 +0800

    added video call

---
+++ lib/main_screens/group/groupChat/group_video_call.dart
@@ -0,0 +1,61 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+
+class VideoCallScreen extends StatefulWidget {
+  const VideoCallScreen({Key? key}) : super(key:key);
+
+  @override
+  State<VideoCallScreen> createState() => _VideoCallScreenState();
+}
+
+class _VideoCallScreenState extends State<VideoCallScreen>
+{
+  final AgoraClient _client = AgoraClient(agoraConnectionData: AgoraConnectionData(
+    appId: '0a8637866ab447e5917d21ab2c681f9c',
+    channelName: 'flutter_study_up',
+    tempToken: '007eJxTYLhscr/8yY2z2VWFN47mn3vn+ILhjRL71Dsze/w1T0zPtbihwGCQaGFmbG5hZpaYZGJinmpqaWieYmSYmGSUbGZhmGaZPCHSMaUhkJGhpaiLlZGBkYEFiEF8JjDJDCZZwKQAQ1pOaUlJalF8cUlpSmV8aQEDAwBQ1CkG',
+  ));
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _initAgora();
+  }
+
+  Future<void> _initAgora() async 
+  {
+    await _client.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Video Call'),
+        ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              AgoraVideoViewer(client: _client,
+              layoutType: Layout.floating,
+              showNumberOfUsers: true,
+              ),
+              AgoraVideoButtons(client: _client,
+              enabledButtons: const [
+                BuiltInButtons.toggleCamera,
+                BuiltInButtons.callEnd,
+                BuiltInButtons.toggleMic,
+              ],),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
