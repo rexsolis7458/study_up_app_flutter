03ecf5345894d3b621df1acc54bf7189a8230f44
maedaguplo <ma.daguplo.20@usjr.edu.ph>
2023-04-27 01:03:47 +0800

    appbar

--- lib/main_screens/home/home_tab.dart
+++ lib/main_screens/home/home_tab.dart
@@ -51,217 +51,206 @@ class HomeTab extends GetWidget<AuthController> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: MainColor,
-          toolbarHeight: 56.0,
-          elevation: 0,
-          title: Container(
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-            decoration: BoxDecoration(
-                color: MainColor,
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
-          ),
-        ),
         body: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(bottom: 10),
-            height: size.height * 0.4,
-            child: Stack(
-              children: <Widget>[
-                // Text(
-                //   'Study Up',
-                //   style: const TextStyle(
-                //     backgroundColor: MainColor,
-                //     color: SecondaryColor,
-                //     fontSize: 40,
-                //     fontWeight: FontWeight.bold,
-                //   ),
-                // ),
-                Container(
-                  height: size.height * 5 - 5,
-                  decoration: BoxDecoration(
-                    color: MainColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(36),
-                      bottomRight: Radius.circular(36),
-                    ),
-                  ),
+      Container(
+        margin: EdgeInsets.only(bottom: 10),
+        height: size.height * 0.4,
+        child: Stack(
+          children: <Widget>[
+            // Text(
+            //   'Study Up',
+            //   style: const TextStyle(
+            //     backgroundColor: MainColor,
+            //     color: SecondaryColor,
+            //     fontSize: 40,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            Container(
+              height: size.height * 5 - 5,
+              decoration: BoxDecoration(
+                color: MainColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(36),
+                  bottomRight: Radius.circular(36),
                 ),
-                // Positioned(
-                //   bottom: 0,
-                //   left: 0,
-                //   right: 0,
-                //   child: Container(
-                //     margin: EdgeInsets.symmetric(horizontal: 16.0),
-                //     height: 54,
-                //     decoration: BoxDecoration(
-                //         color: Colors.white,
-                //         borderRadius: BorderRadius.circular(20),
-                //         boxShadow: [
-                //           BoxShadow(
-                //             offset: Offset(0, 1),
-                //             blurRadius: 80,
-                //             //color: whitewithOpacity(0,23),
-                //           )
-                //         ]),
-                //     child: Column(
-                //       children: [
-                //         Column(
-                //           children: <Widget>[
-                //             Expanded(
-                //               child: TextField(
-                //                 onChanged: (value) {},
-                //                 decoration: InputDecoration(
-                //                   hintText: "Search",
-                //                   hintStyle: TextStyle(
-                //                       color: Colors.black.withOpacity(0.3)),
-                //                   enabledBorder: InputBorder.none,
-                //                   focusedBorder: InputBorder.none,
-                //                 ),
-                //               ),
-                //             ),
-                //           ],
-                //         ),
-                //       ],
-                //     ),
-                //   ),
-                // ),
-              ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text('Stack of Q&A Just For You'),
+            // Positioned(
+            //   bottom: 0,
+            //   left: 0,
+            //   right: 0,
+            //   child: Container(
+            //     margin: EdgeInsets.symmetric(horizontal: 16.0),
+            //     height: 54,
+            //     decoration: BoxDecoration(
+            //         color: Colors.white,
+            //         borderRadius: BorderRadius.circular(20),
+            //         boxShadow: [
+            //           BoxShadow(
+            //             offset: Offset(0, 1),
+            //             blurRadius: 80,
+            //             //color: whitewithOpacity(0,23),
+            //           )
+            //         ]),
+            //     child: Column(
+            //       children: [
+            //         Column(
+            //           children: <Widget>[
+            //             Expanded(
+            //               child: TextField(
+            //                 onChanged: (value) {},
+            //                 decoration: InputDecoration(
+            //                   hintText: "Search",
+            //                   hintStyle: TextStyle(
+            //                       color: Colors.black.withOpacity(0.3)),
+            //                   enabledBorder: InputBorder.none,
+            //                   focusedBorder: InputBorder.none,
+            //                 ),
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Text('Stack of Q&A Just For You'),
 
-          // StreamBuilder<QuerySnapshot>(
-          //   stream: FirebaseFirestore.instance
-          //       .collection('Post/${widget.group['groupName']}/posts')
-          //       .snapshots(),
-          //   builder: (context, snapshot) {
-          //     if (snapshot.connectionState == ConnectionState.waiting) {
-          //       return Center(child: CircularProgressIndicator());
-          //     }
+      // StreamBuilder<QuerySnapshot>(
+      //   stream: FirebaseFirestore.instance
+      //       .collection('Post/${widget.group['groupName']}/posts')
+      //       .snapshots(),
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return Center(child: CircularProgressIndicator());
+      //     }
 
-          //     if (snapshot.hasError) {
-          //       return Center(child: Text('Failed to load posts'));
-          //     }
+      //     if (snapshot.hasError) {
+      //       return Center(child: Text('Failed to load posts'));
+      //     }
 
-          //     final posts = snapshot.data!.docs
-          //         .map((doc) => PostModel.fromFirestore(doc))
-          //         .toList();
-          SingleChildScrollView(
-              child: Container(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                const SizedBox(
-                  height: 20,
-                ),
-                // ListView.builder(
-                // itemCount: posts.length,
-                // itemBuilder: (context, index) {
-                // final post = posts[index];
-                // List<String> upvotes = post.upvoters;
-                // List<String> downvotes = post.downvoters;
+      //     final posts = snapshot.data!.docs
+      //         .map((doc) => PostModel.fromFirestore(doc))
+      //         .toList();
+      SingleChildScrollView(
+          child: Container(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            // ListView.builder(
+            // itemCount: posts.length,
+            // itemBuilder: (context, index) {
+            // final post = posts[index];
+            // List<String> upvotes = post.upvoters;
+            // List<String> downvotes = post.downvoters;
 
-                Container(
-                  width: MediaQuery.of(context).size.width - 40,
-                  height: 130,
-                  // padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    color: BGColor,
-                    // margin: EdgeInsets.symmetric(vertical: 10.0),
-                    child: Column(
-                      // crossAxisAlignment: CrossAxisAlignment.end,
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 130,
+              // padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: BGColor,
+                // margin: EdgeInsets.symmetric(vertical: 10.0),
+                child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding:
+                          const EdgeInsets.only(left: 10, top: 5, right: 30),
+                      width: double.infinity,
+                      child: Text(
+                        'jhhj',
+                        // post.posterName,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 5, right: 30),
+                      width: double.infinity,
+                      child: Text(
+                        'jh',
+                        // '     ${post.title}',
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Text(
+                        'jhjh',
+                        // post.content,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(
-                          padding: const EdgeInsets.only(
-                              left: 10, top: 5, right: 30),
-                          width: double.infinity,
-                          child: Text(
-                            'jhhj',
-                            // post.posterName,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(
-                              left: 20, top: 5, right: 30),
-                          width: double.infinity,
-                          child: Text(
-                            'jh',
-                            // '     ${post.title}',
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          width: double.infinity,
-                          child: Text(
-                            'jhjh',
-                            // post.content,
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Row(
-                              children: [
-                                // IconButton(
-                                //   onPressed: () =>
-                                //       upvotePost(post.id),
-                                //   icon:
-                                //       const Icon(Icons.arrow_upward),
-                                // ),
-                                Text(
-                                  'hjh',
-                                  // '(${post.upvoters.length})',
+                            // IconButton(
+                            //   onPressed: () =>
+                            //       upvotePost(post.id),
+                            //   icon:
+                            //       const Icon(Icons.arrow_upward),
+                            // ),
+                            Text(
+                              'hjh',
+                              // '(${post.upvoters.length})',
+                            ),
+                            // IconButton(
+                            //   onPressed: () =>
+                            //       downvotePost(post.id),
+                            //   icon: const Icon(
+                            //       Icons.arrow_downward),
+                            // ),
+                            Text(
+                              'downvoters',
+                              // '(${post.downvoters.length})',
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 10.0),
+                              child: SizedBox(
+                                width: 180,
+                                height: 40,
+                                child: TextButton(
+                                  onPressed: () {
+                                    // Navigator.push(
+                                    //   context,
+                                    //   MaterialPageRoute(
+                                    //     builder: (context) =>
+                                    //         CommentsPage(
+                                    //             post: post),
+                                    //   ),
+                                    // );
+                                  },
+                                  child: const Text('Comment'),
                                 ),
-                                // IconButton(
-                                //   onPressed: () =>
-                                //       downvotePost(post.id),
-                                //   icon: const Icon(
-                                //       Icons.arrow_downward),
-                                // ),
-                                Text(
-                                  'downvoters',
-                                  // '(${post.downvoters.length})',
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 10.0),
-                                  child: SizedBox(
-                                    width: 180,
-                                    height: 40,
-                                    child: TextButton(
-                                      onPressed: () {
-                                        // Navigator.push(
-                                        //   context,
-                                        //   MaterialPageRoute(
-                                        //     builder: (context) =>
-                                        //         CommentsPage(
-                                        //             post: post),
-                                        //   ),
-                                        // );
-                                      },
-                                      child: const Text('Comment'),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
                           ],
                         ),
                       ],
                     ),
-                  ),
-                )
-              ])))
-        ]));
+                  ],
+                ),
+              ),
+            )
+          ])))
+    ]));
   }
 }
   //             )
