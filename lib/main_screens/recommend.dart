ce32cccc43dcc5bee59cdbfd26a5b8b633d4a79d
rexsolis7458 <rexbryan.solis.20@usjr.edu.ph>
2023-05-08 05:16:40 +0800

    changes

--- lib/main_screens/recommend.dart
+++ lib/main_screens/recommend.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:study_up_app/models/users.dart';
+import 'package:study_up_app/services/database.dart';
 
 class RecommendScreen extends StatefulWidget {
   @override
@@ -17,34 +19,57 @@ class _RecommendScreenState extends State<RecommendScreen> {
   String? _institution;
   List<String> _subjectInterests = [];
 
-  Future<void> _getRecommendations() async {
-    String url = "http://192.168.0.104:5000/recommend";
-    Map<String, dynamic> data = {
-      "age": _age,
-      "gender": _gender,
-      "degree": _degree,
-      "institution": _institution,
-      "subject_interests": _subjectInterests
-    };
-
-    try {
-      http.Response response = await http.post(
-        Uri.parse(url),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode(data),
-      );
-
-      if (response.statusCode == 200) {
-        setState(() {
-          _predictions = List<String>.from(jsonDecode(response.body)["predictions"]);
-        });
-      } else {
-        throw Exception("Failed to get recommendations.");
-      }
-    } catch (e) {
-      print(e);
+  final User? user = FirebaseAuth.instance.currentUser;
+  final UserModel _currentUser = UserModel();
+  void joinGroup(BuildContext context, String groupName) async {
+    String? returnString = await Database().joinGroupByName(groupName, user!.uid);
+    if (returnString == "success") {
     }
   }
+  
+Future<void> _getRecommendations() async {
+  String url = "http://192.168.0.104:5000/recommend";
+  Map<String, dynamic> data = {
+    "age": _age,
+    "gender": _gender,
+    "degree": _degree,
+    "institution": _institution,
+    "subject_interests": _subjectInterests
+  };
+
+  try {
+    http.Response response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      List<String> predictions = List<String>.from(jsonDecode(response.body)["predictions"]);
+
+      // get the collection of groups from Firebase
+      QuerySnapshot groupSnapshot = await FirebaseFirestore.instance.collection("groups").get();
+
+      // loop through each group document and check if its group ID matches a prediction
+      for (QueryDocumentSnapshot groupDoc in groupSnapshot.docs) {
+  String groupId = groupDoc.id;
+  Map<String, dynamic>? data = groupDoc.data() as Map<String, dynamic>?;
+  if (data != null) {
+    String groupName = data["groupName"] as String;
+    if (predictions.contains(groupId)) {
+      setState(() {
+        _predictions.add(groupName);
+      });
+    }
+  }
+}
+    } else {
+      throw Exception("Failed to get recommendations.");
+    }
+  } catch (e) {
+    print(e);
+  }
+}
 
   Future<void> _getUserData() async {
   User? user = FirebaseAuth.instance.currentUser;
@@ -65,17 +90,34 @@ class _RecommendScreenState extends State<RecommendScreen> {
     _getRecommendations();
   }
 
-  Widget _buildPredictionsList() {
-    return ListView.builder(
-      shrinkWrap: true,
+Widget _buildPredictionsList() {
+  return Expanded(
+    child: ListView.builder(
       itemCount: _predictions.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(_predictions[index]),
+        return Card(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  _predictions[index],
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () => joinGroup(context, _predictions[index]),
+                  child: Text('Join'),
+                ),
+              ],
+            ),
+          ),
         );
       },
-    );
-  }
+    ),
+  );
+}
 
   @override
   void initState() {
